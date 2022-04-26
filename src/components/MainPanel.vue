d4d0f8c8ae43730d156d1ba61f9fc1a0c011db63
Can <sxcuican@hotmail.com>
2022-04-27 03:27:32 +0800

    feat(event_handler): 增加事件处理器

--- src/components/MainPanel.vue
+++ src/components/MainPanel.vue
@@ -1,47 +1,16 @@
 <script setup lang="ts">
-import { LCUClient } from '@/lcu';
+import { LCUEventHandler } from '@/lcu';
 import { useCommonStore } from '@/stores';
-import { Event, listen } from '@tauri-apps/api/event';
-import { onBeforeMount, reactive } from 'vue';
-import lodash from 'lodash';
+import { onBeforeMount, reactive, ref } from 'vue';
 
 const commonStore = useCommonStore();
-const state = reactive({
-  isSubscribedWS: false,
-});
-
-const onSelectSession = lodash.debounce((event: LCUClient.IWSEevent) => {
-  console.warn('获取到对局会话', event);
-  const roomId = (event.data.chatDetails.chatRoomName as string).split('@')[0];
-  LCUClient.getRoomMessages(roomId);
-}, 2 * 1000);
-
-const onGameStart = lodash.debounce((event: LCUClient.IWSEevent) => {
-  console.warn('对局开始', event);
-}, 2 * 1000);
-
-function listenWS() {
-  if (state.isSubscribedWS) {
-    return;
-  }
-  state.isSubscribedWS = true;
-
-  listen('on-ws', (e: Event<string>) => {
-    const [, , payload] = JSON.parse(e.payload);
-    const event = payload as LCUClient.IWSEevent;
-
-    if (event.uri == '/lol-champ-select-legacy/v1/implementation-active' && event.data) {
-      onGameStart(event);
-    }
-
-    if (event.uri == '/lol-champ-select/v1/session' && event.eventType == 'Update') {
-      onSelectSession(event);
-    }
-  });
-}
+const eventHandler = ref(new LCUEventHandler());
 
 onBeforeMount(() => {
-  listenWS();
+  eventHandler.value.startListen(['onSelectSession']);
+  eventHandler.value.on('onSelectSession', (event) => {
+    console.log('onSelectSession', event);
+  });
 });
 </script>
 
---
+++ src/lcu/event_handler.ts
@@ -0,0 +1,132 @@
+import lodash from "lodash";
+import { LCUClient } from "./client";
+import { Event, listen } from "@tauri-apps/api/event";
+
+export interface ILCUEventHandlerCallbackEvent<T = any> {
+  name: string;
+  data: T;
+  sourceEvent: LCUClient.IWSEevent;
+}
+
+export type LCUEventHandlerCallback = (
+  event: ILCUEventHandlerCallbackEvent
+) => void;
+
+export type LCUEventHandlerCallbackRecord = {
+  id: number;
+  callback: LCUEventHandlerCallback;
+  eventName: string;
+};
+
+export type ILCUEventHandlerEvent = keyof Omit<
+  { [Key in keyof LCUEventHandler]: any },
+  "on"
+>;
+
+export class LCUEventHandler {
+  protected isSubscribedWS = false;
+  protected listenEvents: string[] = [];
+  protected callbacks: LCUEventHandlerCallbackRecord[] = [];
+
+  /**
+   * 开始监听
+   */
+  startListen(events: ILCUEventHandlerEvent[] = []) {
+    if (this.isSubscribedWS) {
+      console.error("已经在监听LCU事件");
+      return;
+    }
+    this.listenEvents = events;
+    this.isSubscribedWS = true;
+
+    listen("on-ws", (e: Event<string>) => {
+      const [, , payload] = JSON.parse(e.payload);
+      const event = payload as LCUClient.IWSEevent;
+
+      if (event.uri == "/lol-champ-select-legacy/v1/implementation-active") {
+        if (event.data) {
+          this.call("onGameStart", event);
+        } else {
+          this.call("onGameStop", event);
+        }
+      }
+
+      if (
+        event.uri == "/lol-champ-select/v1/session" &&
+        event.eventType == "Update"
+      ) {
+        this.call("onSelectSession", event);
+      }
+    });
+  }
+
+  /**
+   * 增加事件监听
+   * @param event
+   * @param callback
+   */
+  on(event: ILCUEventHandlerEvent, callback: LCUEventHandlerCallback): number {
+    const callbackId = new Date().getTime();
+    this.callbacks.push({
+      id: callbackId,
+      eventName: event,
+      callback,
+    });
+    return callbackId;
+  }
+
+  /**
+   * 防抖调用事件处理函数
+   */
+  protected call = lodash.debounce(
+    async (funcName: ILCUEventHandlerEvent, event: LCUClient.IWSEevent) => {
+      if (
+        (this.listenEvents.length == 0 ||
+          this.listenEvents.includes(funcName)) &&
+        typeof this[funcName] == "function"
+      ) {
+        const data = await (this[funcName] as any)(event);
+
+        // 触发回调消息
+        this.callbacks
+          .filter((it) => it.eventName == funcName)
+          .forEach((it) =>
+            it.callback({
+              name: funcName,
+              data,
+              sourceEvent: event,
+            })
+          );
+      }
+    },
+    2 * 1000
+  );
+
+  /**
+   * 处理对局会话开始
+   */
+  async onSelectSession(
+    event: LCUClient.IWSEevent<{ chatDetails: { chatRoomName: string } }>
+  ) {
+    console.warn("获取到对局会话", event);
+    const roomId = event.data.chatDetails.chatRoomName.split("@")[0];
+    if (roomId) {
+      const roomMessages = await LCUClient.getRoomMessages(roomId);
+      return roomMessages.data;
+    }
+  }
+
+  /**
+   * 处理对局开始
+   */
+  async onGameStart(event: LCUClient.IWSEevent) {
+    console.warn("对局开始", event);
+  }
+
+  /**
+   * 处理对局终止
+   */
+  async onGameStop(event: LCUClient.IWSEevent) {
+    console.warn("对局终止", event);
+  }
+}
